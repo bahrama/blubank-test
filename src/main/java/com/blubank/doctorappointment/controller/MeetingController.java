c35d98bad3c7199fcf03dc490c4ddf7d5788e0c2
Ali The Boss <alialikhahasl@gmail.com>
2023-06-03 09:30:35 +0330

    bt2:validation added

--- src/main/java/com/blubank/doctorappointment/controller/MeetingController.java
+++ src/main/java/com/blubank/doctorappointment/controller/MeetingController.java
@@ -4,12 +4,15 @@ import com.blubank.doctorappointment.dto.MeetingDto;
 import com.blubank.doctorappointment.dto.MeetingDtoManager;
 import com.blubank.doctorappointment.dto.request.ReserveRequest;
 import com.blubank.doctorappointment.dto.response.ApiSuccessResponse;
+import com.blubank.doctorappointment.service.MapValidationErrorService;
 import com.blubank.doctorappointment.service.MeetingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,21 +21,29 @@ public class MeetingController {
 
     private final MeetingService meetingService;
 
+    private final MapValidationErrorService mapValidationErrorService;
+
     @PutMapping
-    public ResponseEntity<?> addMeeting(@RequestBody MeetingDto meetingDto
+    public ResponseEntity<?> addMeeting(@Valid @RequestBody MeetingDto meetingDto
             , BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null) return errorMap;
         meetingService.saveChunk(meetingDto);
         return new ResponseEntity<>(new ApiSuccessResponse(200,HttpStatus.OK),HttpStatus.OK);
     }
 
     @PutMapping("/reserve")
-    public ResponseEntity<?> reserveMeeting(@RequestBody ReserveRequest reserveRequest,BindingResult result){
+    public ResponseEntity<?> reserveMeeting(@Valid @RequestBody ReserveRequest reserveRequest,BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null) return errorMap;
         meetingService.reserveMeeting(reserveRequest);
         return new ResponseEntity<>(new ApiSuccessResponse(200,HttpStatus.OK),HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<?> findMeeting(@RequestBody MeetingDto search,BindingResult result){
+    public ResponseEntity<?> findMeeting(@Valid @RequestBody MeetingDto search,BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null) return errorMap;
         return new ResponseEntity<>(meetingService.findMeetingList(search),HttpStatus.OK);
     }
 
